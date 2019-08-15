66bbb95d8d530f02c950b3a23d11f52f77a725f9
hirohinata <azuri_hirohinata@hotmail.co.jp>
2019-08-15 21:49:00 +0900

    add support for additive expr

--- Calculator/Visitor.fs
+++ Calculator/Visitor.fs
@@ -1,5 +1,46 @@
 ﻿namespace Calculator
 
+open Antlr4.Runtime.Misc
+
 type Visitor() =
     inherit ExpressionBaseVisitor<Result>()
         override this.DefaultResult = Result.Error
+
+        override this.VisitInput([<NotNull>]context: ExpressionParser.InputContext) =
+            context.expr() |> this.Visit
+
+        override this.VisitExpr_additive([<NotNull>]context: ExpressionParser.Expr_additiveContext) =
+            let lhs = context.lhs |> this.Visit
+            let rhs = context.rhs |> this.Visit
+
+            let intOp lhs rhs =
+                match context.op.Type with
+                    | ExpressionParser.PLUS -> (+) lhs rhs
+                    | ExpressionParser.MINUS -> (-) lhs rhs
+                    | _ -> invalidOp ""
+                |> Result.Integer
+            let realOp lhs rhs =
+                match context.op.Type with
+                    | ExpressionParser.PLUS -> (+) lhs rhs
+                    | ExpressionParser.MINUS -> (-) lhs rhs
+                    | _ -> invalidOp ""
+                |> Result.Real
+            let strOp lhs rhs =
+                match context.op.Type with
+                | ExpressionParser.PLUS -> (+) lhs rhs |> String
+                | ExpressionParser.MINUS -> Error
+                | _ -> invalidOp ""
+
+            match lhs, rhs with
+            | Integer lhs, Integer rhs -> intOp lhs rhs
+            | Integer lhs, Real rhs -> realOp (double lhs) rhs
+            | Integer _, (String _ | Error) -> Error
+            | Real lhs, Integer rhs -> realOp lhs (double rhs)
+            | Real lhs, Real rhs -> realOp lhs rhs
+            | Real _, (String _ | Error) -> Error
+            | String lhs, String rhs -> strOp lhs rhs
+            | String _, (Integer _ | Real _ | Error) -> Error
+            | Error, (Integer _ | Real _ | String _ | Error) -> Error
+
+        override this.VisitNum([<NotNull>]context: ExpressionParser.NumContext) =
+            context.UINT().Symbol.Text |> System.Int64.Parse |> Integer
